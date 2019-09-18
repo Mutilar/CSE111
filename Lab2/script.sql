0e865662172105f147d1c0bd45a2559d6561a455
Brian <brianhungerman@gmail.com>
2019-09-18 01:28:11 -0700

    Simple queries on loaded data

---
+++ Lab2/script.sql
@@ -0,0 +1,24 @@
+SELECT * FROM tcph.customer;
+SELECT COUNT(*) FROM tcph.customer;
+SELECT AVG(c_acctbal) FROM tcph.customer;
+
+SELECT * FROM tcph.lineitem;
+SELECT COUNT(*) FROM tcph.lineitem;
+
+SELECT * FROM tcph.nation;
+SELECT COUNT(*) FROM tcph.nation;
+
+SELECT * FROM tcph.orders;
+SELECT COUNT(*) FROM tcph.orders;
+
+SELECT * FROM tcph.part;
+SELECT COUNT(*) FROM tcph.part;
+
+SELECT * FROM tcph.partsupp;
+SELECT COUNT(*) FROM tcph.partsupp;
+
+SELECT * FROM tcph.region;
+SELECT COUNT(*) FROM tcph.region;
+
+SELECT * FROM tcph.supplier;
+SELECT COUNT(*) FROM tcph.supplier;
