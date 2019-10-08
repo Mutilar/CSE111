bf993826bd337e86f2fbb8162214aa4ff06cd008
Brian <brianhungerman@gmail.com>
2019-10-07 19:37:02 -0700

    Fixing 9

--- Lab4/brian-hungerman-lab4/9.sql
+++ Lab4/brian-hungerman-lab4/9.sql
@@ -1,17 +1,19 @@
-SELECT SUM(count)
-FROM supplier
-INNER JOIN (
-	SELECT l_suppkey, COUNT(*) count
+SELECT COUNT(*)
+FROM (
+	SELECT o_clerk
 	FROM lineitem
 	INNER JOIN (
-		SELECT o_orderkey
+		SELECT s_suppkey
+		FROM supplier
+		WHERE s_nationkey IN (
+			SELECT n_nationkey
+			FROM nation
+			WHERE n_name = 'RUSSIA'
+		)
+	) l ON s_suppkey = l_suppkey
+	INNER JOIN (
+		SELECT o_clerk, o_orderkey
 		FROM orders
-        GROUP BY o_clerk
-	) o ON l_orderkey = o_orderkey
-    GROUP BY l_suppkey
-) l ON s_suppkey = l_suppkey
-WHERE s_nationkey IN (
-	SELECT n_nationkey
-    FROM nation
-    WHERE n_name = 'RUSSIA'
+	) o ON o_orderkey = l_orderkey
+	GROUP BY o_clerk
 )
