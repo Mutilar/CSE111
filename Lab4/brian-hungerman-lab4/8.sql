1f1315a89c330484cf632725003501c47a14ebc7
Brian <brianhungerman@gmail.com>
2019-10-07 20:44:19 -0700

    Changing 8 logic (still wrong lol)

--- Lab4/brian-hungerman-lab4/8.sql
+++ Lab4/brian-hungerman-lab4/8.sql
@@ -1,21 +1,21 @@
 SELECT n_name, COUNT(*)
-FROM orders
+FROM lineitem
 INNER JOIN (
-	SELECT l_orderkey, n_name
-	FROM lineitem
+	SELECT s_suppkey, n_name
+	FROM supplier
 	INNER JOIN (
-		SELECT s_suppkey, n_name
-		FROM supplier
-		INNER JOIN (
-			SELECT n_nationkey, n_name
-			FROM nation
-			WHERE n_regionkey = (
-				SELECT r_regionkey
-				FROM region
-				WHERE r_name = 'AMERICA'
-			)
-		) n ON s_nationkey = n_nationkey
-	) s on l_suppkey = s_suppkey	
+		SELECT n_nationkey, n_name
+		FROM nation
+		WHERE n_regionkey = (
+			SELECT r_regionkey
+			FROM region
+			WHERE r_name = 'AMERICA'
+		)
+	) n ON s_nationkey = n_nationkey
+) s on l_suppkey = s_suppkey	
+INNER JOIN (
+	SELECT o_orderkey
+	FROM orders
+	WHERE o_orderstatus = 'F' AND YEAR(o_orderdate) = 1995
 ) l ON o_orderkey = l_orderkey
-WHERE o_orderstatus = 'F' AND YEAR(o_orderdate) = 1995
 GROUP BY n_name
