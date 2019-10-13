2dd1776be923075aa65f223b344a90281c2180e8
Brian <brianhungerman@gmail.com>
2019-10-13 15:29:27 -0700

    Finished 9

---
+++ Lab5/brian-hungerman-lab5/9.sql
@@ -0,0 +1,17 @@
+
+SELECT COUNT(*)
+FROM (
+	SELECT (ps_supplycost * ps_availqty) val, ps_suppkey
+	FROM partsupp
+	ORDER BY val DESC
+	LIMIT ((SELECT COUNT(*) FROM partsupp) * .03)
+)
+INNER JOIN (
+	SELECT s_suppkey
+	FROM supplier
+	INNER JOIN (
+		SELECT n_nationkey
+		FROM nation
+		WHERE n_name = 'CANADA'
+	) n ON s_nationkey = n_nationkey
+) s ON s_suppkey = ps_suppkey
