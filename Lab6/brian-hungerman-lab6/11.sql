0ab328459d85dbadfb9b2cc944950888d7573f54
Brian <brianhungerman@gmail.com>
2019-10-29 17:16:33 -0700

    Finished 11

---
+++ Lab6/brian-hungerman-lab6/11.sql
@@ -0,0 +1,16 @@
+SELECT (
+    SELECT n_name 
+    FROM nation
+    WHERE n_nationkey = c_nationkey
+)
+FROM customer
+GROUP BY c_nationkey
+HAVING COUNT(*) = (
+    SELECT MAX(num)
+    FROM
+    ( 
+        SELECT c_nationkey, COUNT(*) num
+        FROM customer
+        GROUP BY c_nationkey
+    )
+)
